9d0f64013f66158672859733618407557f54acd0
Diogo Pires <diogo.pires2211@gmail.com>
2024-08-08 12:02:16 +0100

    minimal commit, just so i dont have to do it again on main computer

--- src/app/components/navbar/page.js
+++ src/app/components/navbar/page.js
@@ -1,15 +1,23 @@
 import styles from "./navbar.module.sass";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 
 
 export default function Navbar() {
     return (
         <div className={styles.navBar}>
-            <a href="#home" className={styles.navlink}>Home</a>
-            <a href="#projects" className={styles.navlink}>Projects</a>
-            <a href="#about" className={styles.navlink}>About me</a>
-            {/* TODO: <FontAwesomeIcon icon="fas fa-search" /> */}
-            <button className={styles.accountButton}>Account</button>
+            <div className={styles.itemsDiv}>
+                <a href="#home" className={styles.navLink}>Home</a>
+                <a href="#projects" className={styles.navLink}>Projects</a>
+                <a href="#about" className={styles.navLink}>About me</a>
+            </div>
+
+            <div className={styles.accountDiv}>
+                {/* TODO: <FontAwesomeIcon icon="fas fa-search" /> */}
+                <FontAwesomeIcon icon={faSearch} className={styles.searchButton}/>
+                <button className={styles.accountButton}>Account</button>
+            </div>
         </div>
     );
 }
