43492aa966ccd96fef4516ada5dc422914caaafb
Diogo Pires <diogo.pires2211@gmail.com>
2024-08-09 23:21:11 +0100

    home page design almost done. backend TODO

--- src/app/components/navbar/page.js
+++ src/app/components/navbar/page.js
@@ -12,7 +12,7 @@ const archivo = Archivo({
 
 export default function Navbar() {
     return (
-        <div className={styles.navBar}>
+        <div className={`${styles.navBar} ${archivo.className}`}>
             <div className={styles.itemsDiv}>
                 <a href="#home" className={styles.navLink}>Home</a>
                 <a href="#projects" className={styles.navLink}>Projects</a>
@@ -20,9 +20,8 @@ export default function Navbar() {
             </div>
 
             <div className={styles.accountDiv}>
-                {/* TODO: <FontAwesomeIcon icon="fas fa-search" /> */}
                 <FontAwesomeIcon icon={faSearch} className={styles.searchButton}/>
-                <button className={styles.accountButton}>Account</button>
+                <a className={styles.accountButton} href="#dashboard"><span>Account</span></a>
             </div>
         </div>
     );
