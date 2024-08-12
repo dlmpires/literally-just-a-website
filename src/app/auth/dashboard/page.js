3a4978ee0b758ee2d3ba8f3f117785230b7bc6da
Diogo Pires <diogo.pires2211@gmail.com>
2024-08-12 19:47:22 +0100

    logout and dashboard done. TODO: DESIGN OF ALL AUTH

--- src/app/auth/dashboard/page.js
+++ src/app/auth/dashboard/page.js
@@ -1,17 +1,41 @@
 'use client'
 import styles from "./dashboard.module.sass";
 import Navbar from "../../components/navbar/page.js"
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 import dotenv from 'dotenv'
 
+dotenv.config()
 const server_url = process.env.NEXT_PUBLIC_SERVER_URL
 
 export default function Dashboard() {
+
+  const [message,setMessage] = useState('')
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleDashboard = async () => {
+      const res = await fetch(`${server_url}/dashboard`, {
+        method: 'GET',
+        credentials: 'include'
+      })
+
+      if (res.status === 401) {
+        router.push('login')
+      } else {
+        const data = await res.json()
+        setMessage(data.message)
+      }
+    }
+    handleDashboard()
+  })
+
   return (
     <main className={styles.main}>
       <Navbar/>
       <div className={styles.Content}>
         <h1>YOU ARE ON YOUR DASHBOARD :))</h1>
+        <h1>{message || 'Loading...'}</h1> {/* Display loading text until message is fetched */}
 
         <div>
           <img className={styles.wip} src="/home/WIP.png" alt="Work in Process"/>
--- src/app/components/navbar/page.js
+++ src/app/components/navbar/page.js
@@ -1,16 +1,38 @@
+'use client'
 import styles from "./navbar.module.sass";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { Archivo } from '@next/font/google';
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 const archivo = Archivo({
     weight: ['400', '600'], // Specify the font weights you want to use
     subsets: ['latin'], // Choose the subsets you need
-  });
-  
-  
+});
+const server_url = process.env.NEXT_PUBLIC_SERVER_URL
+
+
 
 export default function Navbar() {
+    const [message, setMessage] = useState()
+    const router = useRouter()
+
+    const handleLogout = async () => {
+        const res = await fetch(`${server_url}/logout`, {
+            method: 'GET',
+            credentials: 'include'
+        })
+        const data = await res.text();
+        setMessage(data);
+        if(res.ok) {
+            console.log('logout triggered frontend')
+            router.push('/')
+        } else {
+            
+        }
+    }
+
     return (
         <div className={`${styles.navBar} ${archivo.className}`}>
             <div className={styles.itemsDiv}>
@@ -19,8 +41,9 @@ export default function Navbar() {
                 <a href="#about" className={styles.navLink}>About me</a>
             </div>
 
+            <button onClick={handleLogout}>logout</button>
             <div className={styles.accountDiv}>
-                <FontAwesomeIcon icon={faSearch} className={styles.searchButton}/>
+                <FontAwesomeIcon icon={faSearch} className={styles.searchButton} />
                 <a className={styles.accountButton} href="/auth/register"><span>Account</span></a>
             </div>
         </div>
